6cb55f8a8213d48b9d820eb7b1f3e1ac8d01042e
Harsh Pundir <Harsh Pundir@DESKTOP-KI3K0FJ.mshome.net>
2022-03-26 10:39:07 +0530

    1st Git Commit

---
+++ Project_01/src/System/out/P2_Task02_KeywordsInJavaInTerminal.java
@@ -0,0 +1,26 @@
+//Keywords are the reserved words in JAVA which cannot be used in programming for any other purposes.
+
+package System.out;
+
+public class P2_Task02_KeywordsInJavaInTerminal {
+
+	public static void main(String[] args) {
+		void int() //error invalid name
+		{
+		System.out.println("Hi ! This is our Practical 2's Task 2: Keywords in Java");
+		}
+	}
+	//variable
+	int double= 90; //Syntax error n token "double" ,Invalid Variable declaration
+	
+	//Object
+	ABC int= new ABC(): //Again error as we are creating object name as int.
+	
+}
+
+class ABC{
+	ABC()
+	{
+	System.out.println("I'm class ABC");
+	}
+} 
---
+++ Project_01/src/Unit_01/P2_Task02_ConstantsInJava.java
@@ -0,0 +1,19 @@
+package Unit_01;
+
+public class P2_Task02_ConstantsInJava {
+
+private static final double Pi = 3.14;
+public static void main(String[] args) {
+	final int a= 10;
+	// a= 11;
+	System.out.println(Pi);
+	
+	System.out.println(a);
+	
+	add(1, 2);
+	multi(1, 2);
+	
+	Calculator
+}
+
+	}
